781e821168582d025db842bd179fd2ac902e77a9
izziperron <izziperron@gmail.com>
2018-07-24 15:52:27 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -2,7 +2,13 @@ def breakfest(meal_choice = "frosted flakes")
   puts "Morning is the best time for #{meal_choice}!"
 end
 
-def lunch(mean_choice = )
+def lunch(meal_choice = "grilled cheese")
+  puts "The best meal for lunch is definitly #{meal_choice}!"
+end
+
+def dinner(meal_choice = "salmon")
+  puts "You should eat #{meal_choice} for dinner tonight!"
+end
 
 # Call the methods with puts and your own arguments here. Like this:
 puts snacks("Swedish fish")
